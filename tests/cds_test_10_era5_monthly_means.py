69d9dcc79e58ff428ef590222f0e8e46ac92253b
Marco Cucchi <m.cucchi@bopen.eu>
2020-10-19 12:41:19 +0200

    adds cds tests for era5 monthly means

---
+++ tests/cds_test_10_era5_monthly_means.py
@@ -0,0 +1,138 @@
+import pytest
+
+import cfgrib
+import cfgrib.xarray_store
+
+import cdscommon
+
+
+TEST_FILES = {
+    'era5-single-levels-monthly-means-monthly_averaged_reanalysis': [
+        'reanalysis-era5-single-levels-monthly-means',
+        {
+            'format': 'grib',
+            'product_type': 'monthly_averaged_reanalysis',
+            'variable': '2m_temperature',
+            'year': '2017',
+            'month': ['01', '02'],
+            'time': '00:00',
+        },
+        190,
+    ],
+    'era5-single-levels-monthly-means-monthly_averaged_reanalysis_by_hour_of_day': [
+        'reanalysis-era5-single-levels-monthly-means',
+        {
+            'format': 'grib',
+            'product_type': 'monthly_averaged_reanalysis_by_hour_of_day',
+            'variable': '2m_temperature',
+            'year': '2017',
+            'month': ['01', '02'],
+            'time': '00:00',
+        },
+        190,
+    ],
+    'era5-single-levels-monthly-means-monthly_averaged_ensemble_members': [
+        'reanalysis-era5-single-levels-monthly-means',
+        {
+            'format': 'grib',
+            'product_type': 'monthly_averaged_ensemble_members',
+            'variable': '2m_temperature',
+            'year': '2017',
+            'month': ['01', '02'],
+            'time': '00:00',
+        },
+        192,
+    ],
+    'era5-single-levels-monthly-means-monthly_averaged_ensemble_members_by_hour_of_day': [
+        'reanalysis-era5-single-levels-monthly-means',
+        {
+            'format': 'grib',
+            'product_type': 'monthly_averaged_ensemble_members_by_hour_of_day',
+            'variable': '2m_temperature',
+            'year': '2017',
+            'month': ['01', '02'],
+            'time': '00:00',
+        },
+        192,
+    ],
+    'era5-pressure-levels-monthly-means-monthly_averaged_reanalysis': [
+        'reanalysis-era5-pressure-levels-monthly-means',
+        {
+            'product_type': 'monthly_averaged_reanalysis',
+            'variable': 'divergence',
+            'pressure_level': '1',
+            'year': '2017',
+            'month': ['01', '02'],
+            'time': '00:00',
+        },
+        190,
+    ],
+    'era5-pressure-levels-monthly-means-monthly_averaged_reanalysis_by_hour_of_day': [
+        'reanalysis-era5-pressure-levels-monthly-means',
+        {
+            'product_type': 'monthly_averaged_reanalysis_by_hour_of_day',
+            'variable': 'divergence',
+            'pressure_level': '1',
+            'year': '2017',
+            'month': ['01', '02'],
+            'time': '00:00',
+        },
+        190,
+    ],
+    'era5-pressure-levels-monthly-means-monthly_averaged_ensemble_members': [
+        'reanalysis-era5-pressure-levels-monthly-means',
+        {
+            'product_type': 'monthly_averaged_ensemble_members',
+            'variable': 'divergence',
+            'pressure_level': '1',
+            'year': '2017',
+            'month': ['01', '02'],
+            'time': '00:00',
+        },
+        192,
+    ],
+    'era5-pressure-levels-monthly-means-monthly_averaged_ensemble_members_by_hour_of_day': [
+        'reanalysis-era5-pressure-levels-monthly-means',
+        {
+            'product_type': 'monthly_averaged_ensemble_members_by_hour_of_day',
+            'variable': 'divergence',
+            'pressure_level': '1',
+            'year': '2017',
+            'month': ['01', '02'],
+            'time': '00:00',
+        },
+        192,
+    ],
+}
+
+
+@pytest.mark.parametrize('test_file', TEST_FILES.keys())
+def test_Stream(test_file):
+    dataset, request, key_count = TEST_FILES[test_file]
+    path = cdscommon.ensure_data(dataset, request, name='cds-' + test_file + '-{uuid}.grib')
+
+    stream = cfgrib.FileStream(path)
+    leader = stream.first()
+    assert len(leader) == key_count
+    assert sum(1 for _ in stream) == leader['count']
+
+
+@pytest.mark.parametrize('test_file', TEST_FILES.keys())
+def test_Dataset(test_file):
+    dataset, request, key_count = TEST_FILES[test_file]
+    path = cdscommon.ensure_data(dataset, request, name='cds-' + test_file + '-{uuid}.grib')
+
+    res = cfgrib.xarray_store.open_dataset(path)
+    res.to_netcdf(path[:-5] + '.nc')
+
+
+@pytest.mark.skip()
+def test_large_Dataset():
+    dataset, request, key_count = TEST_FILES['era5-pressure-levels-ensemble_members']
+    # make the request large
+    request['day'] = list(range(1, 32))
+    request['time'] = list(['%02d:00' % h for h in range(0, 24, 3)])
+    path = cdscommon.ensure_data(dataset, request, name='cds-' + dataset + '-LARGE-{uuid}.grib')
+
+    res = cfgrib.xarray_store.open_dataset(path)
+    res.to_netcdf(path[:-5] + '.nc')
